61c901eb7f4790309e6a6c3b97e1443f9ab2be18
madewgn <mad.ewgn2@gmail.com>
2022-09-20 09:00:06 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import replNeverSleep
+replNeverSleep.awake('https://NeverSleep.c4syner.repl.co', True) #Param 1: Link to this repl's website, Param 2: Debug Set to true (print status code for the request, good=200)
+#Thats it! 
+#Feel free to code below without your program ever going to sleep!
 import os
 key = "IG_USERNAME"
-value = os.getenv(key)
+value = "made_wgn"
 while True:
     os.system(f"python3 qlizz.py {value}")
-
